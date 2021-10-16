714635ebb5ef2af1bf082c882a0cbb6996706955
KK <ery.eden@gmail.com>
2021-10-16 22:39:28 +0900

    PIS-5 Add pod example body.

---
+++ gke-sample/example/k8s_pod.py
@@ -0,0 +1,14 @@
+from gke_sample.k8s_utils.persistent_volume import *
+
+
+def main():
+
+    pod_name = "pd-feeder-pod"
+    volume_name = "nice-pvc"
+    pvc_name = "pv-test1"
+
+    containers = ""
+
+
+if __name__ == '__main__':
+    main()
